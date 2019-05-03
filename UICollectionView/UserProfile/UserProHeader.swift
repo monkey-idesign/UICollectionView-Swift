525f605b97bdd61a6ec1fee809b65355657a9a33
Samreaksa Ros <15318223+monkeyidesign@users.noreply.github.com>
2019-05-03 13:59:59 +0700

    Added user image as profile.

--- UICollectionView/UserProfile/UserProHeader.swift
+++ UICollectionView/UserProfile/UserProHeader.swift
@@ -14,7 +14,13 @@ class UserProHeader: UICollectionViewCell {
     let profileImageView: UIImageView = {
         
         let iv = UIImageView()
-        iv.backgroundColor = .red
+        iv.backgroundColor = UIColor.init(red: 255/255, green: 255/255, blue: 255/255, alpha: 0.8)
+        
+        iv.image = UIImage(named: "profile_selected")
+        iv.clipsToBounds = true
+        iv.layer.cornerRadius = 10
+        //iv.layer.cornerRadius = 60 / 2
+    
         return iv
         
     }()
@@ -22,9 +28,11 @@ class UserProHeader: UICollectionViewCell {
     override init(frame: CGRect) {
         super.init(frame: frame)
         
-        backgroundColor = .green
+        backgroundColor = UIColor.rgb(red: 68, green: 142, blue: 246)
         
         addSubview(profileImageView)
+        profileImageView.anchor(top: topAnchor, left: self.leftAnchor, bottom: nil, right: nil, paddingTop: 12, paddingLeft: 12, paddingBottom: 0, paddingRight: 12, width: 60, height: 60)
+        
     }
     
     required init?(coder aDecoder: NSCoder) {
